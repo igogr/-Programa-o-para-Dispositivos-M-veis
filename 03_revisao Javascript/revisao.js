db8abc6795b71cb27255e39592a1906e63d8b817
igogr <igor.peixoto@iesb.edu.br>
2025-03-07 19:27:34 -0300

    incluindo estruturas de controle

--- 03_revisao Javascript/revisao.js
+++ 03_revisao Javascript/revisao.js
@@ -58,18 +58,28 @@ const menor = 2 < 3 // false
 const maiorOuIgual = 3 >= 2 // true
 const menorOuIgual = 2 <= 3 //false
 
-/*Estruturas de controle 
-if(idade >= 18) {
-    console.log("Maior de Idade")
+idade = 10
+
+// Estruturas de controle
+if (idade >= 18) {
+  console.log("Maior de Idade")
 }
-if(idade >= 18){
-    console.log("Maior de Idade")
+
+if (idade >= 18) {
+  console.log("Maior de Idade")
 } else {
-    console.log("Menor de Idade")
+  console.log("Menor de idade")
 }
-*/
+
+if (idade >= 18) {
+  console.log("Maior de idade")
+} else if(idade < 18) {
+  console.log("Menor de idade")
+} else {
+  console.log("Valor inválido")
+}
 
 
-//Operador Ternario
-
-const checkMaiorDeIdade = idade >=
+// Operador ternário
+const checkMaiorDeIdade = idade >= 18 ? "Maior de idade" : "Menor de idade";
+console.log(checkMaiorDeIdade)
